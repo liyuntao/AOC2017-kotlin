89f2ba4506c06e68e1940fe1fba48dfd5dca7657
liyuntao <liyuntao@finogeeks.com>
2018-02-17 17:11:03 +0800

    refactor 01

--- src/main/kotlin/com/liyuntao/aoc/day01.kt
+++ src/main/kotlin/com/liyuntao/aoc/day01.kt
@@ -2,26 +2,25 @@ package com.liyuntao.aoc
 
 import java.io.File
 
-class Day01 {
+object Day01 {
     fun q1(input: String) {
         (input + input[0]).asSequence()  // "1122" -> "11221" -> ['1', '1', '2', '2', '1']
                 .windowed(2) // [ ['1', '1'] ['1', '2'] ['2', '2']  ['2', '1'] ]
                 .filter { it[0] == it[1] } // [ ['1', '1'], ['2', '2'] ]
                 .map { Character.getNumericValue(it[0]) } // [1,2]
                 .sum() // 3
-                .let { println(it) }
+                .let { println("q1 result $it") }
     }
 
     fun q2(input: String) {
         (0 until input.length).map { if (input[it] == input[(it + input.length / 2) % (input.length)]) Character.getNumericValue(input[it]) else 0 }
                 .sum()
-                .let { println(it) }
+                .let { println("q2 result $it") }
     }
 }
 
 fun main(args: Array<String>) {
-    val day01 = Day01()
     val input = File("./inputs/day01.txt").readText()
-    day01.q1(input)
-    day01.q2(input)
+    Day01.q1(input)
+    Day01.q2(input)
 }
